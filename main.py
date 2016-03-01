33002ecc749733975a81af85454264cce7f253d5
kitsune-udon <kitsune.udon.delicious@gmail.com>
2016-03-01 15:13:07 +0900

    fix

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def calc_and_plot(title, func_true, x_min, x_max, x_delta, noise_sigma,
         func_lsm   = lambda x: np.poly1d(coef_lsm)(x)
         plot_curve(func_lsm, "LSM(d=%d)" % (d), x_min, x_max, 0.01)
 
-    coef_lasso = estimate_by_lasso(xs,ys,20)
+    coef_lasso = estimate_by_lasso(xs,ys,lasso_dim)
     func_lasso = lambda x: np.poly1d(coef_lasso)(x)
     plot_curve(func_lasso, "Lasso(d=%d)" % (lasso_dim), x_min, x_max, 0.01)
 
@@ -74,7 +74,7 @@ func_true  = lambda x: -math.pow(x,10)+x
 func_true2 = lambda x: math.sin(x)
 
 np.random.seed(0)
-calc_and_plot(r"$-x^{10}+x$", func_true, 0., 1., 0.05, 0.05, subplot_param=(2,1,1))
+calc_and_plot(r"$-x^{10}+x$", func_true, 0., 1., 0.05, 0.05, subplot_param=(2,1,1), lasso_dim=20)
 calc_and_plot(r"$\sin x$", func_true2, 0., math.pi*2, 0.2, 0.001, subplot_param=(2,1,2), ylim=(-1.3, 1.3), legend_loc='lower left', lsm_dims=[4,6])
 
 plt.savefig('image.png')
