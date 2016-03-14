e35f1d90bac4f2a9ea6ca75d597025f09296b0c0
kitsune-udon <kitsune.udon.delicious@gmail.com>
2016-03-15 00:58:46 +0900

    refactoring

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random,math
 
-algorithm = 'spams_lars'
+algorithm = 'my_fista'
 
 if algorithm in ['sklearn_cd']:
     from sklearn import linear_model
@@ -148,32 +148,31 @@ def estimate_by_lasso(xs, ys, dim, lambda1):
     else:
         raise "Specify Algorithm"
 
-def calc_and_plot(title, func_true, x_min, x_max, x_delta, noise_sigma,
-        lsm_dims=[2,10], lasso_dim=20, lasso_lambda=0.1, ylim=None, legend_loc='upper left',
-        subplot_param=None):
-    if subplot_param:
-        plt.subplot(*subplot_param)
+def calc_and_plot(plot_param):
+    if plot_param["subplot_param"]:
+        plt.subplot(*plot_param["subplot_param"])
 
-    if ylim:
-        plt.ylim(*ylim)
+    if plot_param["ylim"]:
+        plt.ylim(*plot_param["ylim"])
 
-    plt.title(title)
-    xs = np.arange(x_min, x_max, x_delta)
-    ys = generate_values(xs, func_true, noise_sigma)
+    plt.title(plot_param["title"])
+    x_min, x_max = plot_param["xlim"]
+    xs = plot_param["x_gen"](x_min, x_max)
+    ys = generate_values(xs, plot_param["func_true"], plot_param["noise_sigma"])
 
     plot_dots(xs,ys)
-    plot_curve(func_true, 'True', x_min, x_max, 0.01)
+    plot_curve(plot_param["func_true"], 'True', x_min, x_max, 0.01)
 
-    for d in lsm_dims:
+    for d in plot_param["lsm_dims"]:
         coef_lsm   = estimate_by_lsm(xs, ys, d)
         func_lsm   = lambda x: np.poly1d(coef_lsm)(x)
-        plot_curve(func_lsm, "LSM(d=%d)" % (d), x_min, x_max, 0.01)
+        plot_curve(func_lsm, "LSM(d={})".format(d), x_min, x_max, 0.01)
 
-    coef_lasso = estimate_by_lasso(xs,ys,lasso_dim,lasso_lambda)
+    coef_lasso = estimate_by_lasso(xs,ys,plot_param["lasso_dim"],plot_param["lasso_lambda"])
     func_lasso = lambda x: np.poly1d(coef_lasso)(x)
-    plot_curve(func_lasso, "Lasso(d=%d)" % (lasso_dim), x_min, x_max, 0.01)
+    plot_curve(func_lasso, "Lasso(d={})".format(plot_param["lasso_dim"]), x_min, x_max, 0.01)
 
-    plt.legend(loc=legend_loc)
+    plt.legend(loc=plot_param["legend_loc"])
 
 
 plt.figure(num=None, figsize=(8,12), dpi=80)
@@ -182,8 +181,36 @@ func_true  = lambda x: -math.pow(x,10)+x
 func_true2 = lambda x: math.sin(x)
 
 np.random.seed(1)
-lasso_lambda = 1e-30
-calc_and_plot(r"$-x^{10}+x$", func_true, 0., 1., 0.05, 0.05, subplot_param=(2,1,1), ylim=(0.0, 1.0), lasso_dim=20, lasso_lambda=lasso_lambda)
-calc_and_plot(r"$\sin x$", func_true2, 0., math.pi*2, 0.2, 0.05, subplot_param=(2,1,2), ylim=(-1.3, 1.3), legend_loc='lower left', lsm_dims=[4,6], lasso_dim=20, lasso_lambda=lasso_lambda)
+lasso_lambda = 1e-3
+plot_param = {
+        "title" : r"$-x^{10}+x$",
+        "xlim" : (0., 1.),
+        "x_gen" : lambda x_min, x_max: np.arange(x_min, x_max, 0.05),
+        "noise_sigma" : 0.05,
+        "subplot_param" : (2,1,1),
+        "ylim" : (0., 1.),
+        "func_true" : func_true,
+        "lasso_dim" : 2,
+        "lasso_lambda" : lasso_lambda,
+        "legend_loc" : "upper left",
+        "lsm_dims" : [4,6],
+        "lasso_dim" : 20,
+        }
+plot_param2 = {
+        "title" : r"$\sin x$",
+        "xlim" : (0.,  math.pi),
+        "x_gen" : lambda x_min, x_max: np.arange(x_min, x_max, 0.1),
+        "noise_sigma" : 0.0001,
+        "subplot_param" : (2,1,2),
+        "ylim" : (-1.3, 1.3),
+        "func_true" : func_true2,
+        "lasso_dim" : 2,
+        "lasso_lambda" : 1e-50,
+        "legend_loc" : "lower left",
+        "lsm_dims" : [4,6],
+        "lasso_dim" : 20,
+        }
+calc_and_plot(plot_param)
+calc_and_plot(plot_param2)
 
 plt.savefig('image.png')
